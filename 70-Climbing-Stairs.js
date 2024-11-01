31bb7f7897f59d8e33d4f20ffdbee2046a6dbe79
Farhana Khan <134283374+farhanakhanswe@users.noreply.github.com>
2024-11-01 23:28:06 +0600

    Create 70-Climbing-Stairs.js

---
+++ 70-Climbing-Stairs.js
@@ -0,0 +1,14 @@
+/* Question Link: https://neetcode.io/problems/climbing-stairs */
+
+/* can't come up with a solution yet 
+
+  // let's imagine we are at step 1 and the top is step 10
+  // if we only climb 1 step then total number of steps would be n - 1
+  // if we only climb 2 steps and the top number is odd then the total number of steps would be n - 2/ 2
+
+  // n is = or > 1 so there will always be atleast 1 way to reach the top
+  // if n is odd number then there will always be 1 way to reach the top by n - 2/2 steps
+  // if n is an even number then there will always be 1 way to reach the top by n steps
+  // 
+*/
+
