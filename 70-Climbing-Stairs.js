8abf1d0f251a03f07e2d96fe42df76b78eb3883b
Farhana Khan <134283374+farhanakhanswe@users.noreply.github.com>
2024-11-02 23:58:32 +0600

    Update 70-Climbing-Stairs.js

--- 70-Climbing-Stairs.js
+++ 70-Climbing-Stairs.js
@@ -1,14 +1,14 @@
-/* Question Link: https://neetcode.io/problems/climbing-stairs */
+/* Question Link: https://leetcode.com/problems/climbing-stairs/ */
 
-/* can't come up with a solution yet 
+var climbStairs = function(n) { 
+    let dp = [];
+    dp[1] = 1;
+    dp[2] = 2;
 
-  // let's imagine we are at step 1 and the top is step 10
-  // if we only climb 1 step then total number of steps would be n - 1
-  // if we only climb 2 steps and the top number is odd then the total number of steps would be n - 2/ 2
+    for(let i = 3; i <= n ; i++)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
 
-  // n is = or > 1 so there will always be atleast 1 way to reach the top
-  // if n is odd number then there will always be 1 way to reach the top by n - 2/2 steps
-  // if n is an even number then there will always be 1 way to reach the top by n steps
-  // 
-*/
-
+    return dp[n];
+};
